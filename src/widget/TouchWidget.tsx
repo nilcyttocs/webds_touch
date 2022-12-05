4365dee129d70c2ec5c4a48856a6e4139414520c
Scott Lin <scott.lin@tw.synaptics.com>
2022-12-05 13:13:10 +0800

    Consolidate div elements for widget styling

--- src/widget/TouchWidget.tsx
+++ src/widget/TouchWidget.tsx
@@ -18,12 +18,8 @@ export class TouchWidget extends ReactWidget {
 
   render(): JSX.Element {
     return (
-      <div id={this.id + "_container"} className="jp-webds-widget-container">
-        <div id={this.id + "_content"} className="jp-webds-widget">
-          <TouchComponent service={this.service} />
-        </div>
-        <div className="jp-webds-widget-shadow jp-webds-widget-shadow-top"></div>
-        <div className="jp-webds-widget-shadow jp-webds-widget-shadow-bottom"></div>
+      <div id={this.id + "_component"}>
+        <TouchComponent service={this.service} />
       </div>
     );
   }
