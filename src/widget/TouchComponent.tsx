1c2087bc6b98b5ca05788fd6c459f338be2b0b25
Scott Lin <scott.lin@tw.synaptics.com>
2022-12-26 13:47:11 +0800

    Update local exports

--- src/widget/TouchComponent.tsx
+++ src/widget/TouchComponent.tsx
@@ -8,15 +8,13 @@ import { ThemeProvider } from "@mui/material/styles";
 
 import Landing from "./Landing";
 
-import { webdsService } from "./local_exports";
+import { requestAPI, webdsService } from "./local_exports";
 
 import {
   ALERT_MESSAGE_APP_INFO,
   ALERT_MESSAGE_ADD_PUBLIC_CONFIG_JSON,
   ALERT_MESSAGE_ADD_PRIVATE_CONFIG_JSON
 } from "./constants";
-
-import { requestAPI } from "../handler";
 
 let alertMessage = "";
 
